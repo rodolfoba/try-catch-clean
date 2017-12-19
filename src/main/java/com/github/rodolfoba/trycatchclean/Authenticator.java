801ad4ff61a93305c19349f9200e86539b1408e6
rodolfoba <rodolfoba87@gmail.com>
2017-12-18 23:22:35 -0200

    Versao inicial

--- src/main/java/com/github/rodolfoba/trycatchclean/Authenticator.java
+++ src/main/java/com/github/rodolfoba/trycatchclean/Authenticator.java
@@ -3,10 +3,12 @@ package com.github.rodolfoba.trycatchclean;
 class Authenticator {
 
     public User login(String id, String pwd) throws Exception {
-        return new User(id, "User Name");
+        throw new Exception("Login exception");
+//        return new User(id, "User Name");
     }
     
     public User gmailLogin(String id, String pwd) throws Exception {
+//        throw new Exception("GmailLogin exception");
         return new User(id, "User Name");
     }
     
